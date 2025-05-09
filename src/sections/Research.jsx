ba0c83013aa6bf83c8c656be2f27686fa85217cf
MunawirRifai <158122281+MunawirRifai@users.noreply.github.com>
2025-05-09 08:35:50 +0700

    Update Research.jsx

--- src/sections/Research.jsx
+++ src/sections/Research.jsx
@@ -10,7 +10,7 @@ function Research(){
                 <img src={researchIamage} alt="penelitian" className='section-image' />
                 <div className="text">
                     <h1>Penelitian</h1>
-                    <p>Seorang peneliti sinyal/frekuensi merupakan profesional yang berfokus pada pengkajian, analisis, dan pengembangan teknologi berbasis gelombang elektromagnetik, sinyal analog, dan sinyal digital. Bidang penelitian ini mencakup berbagai aplikasi, seperti telekomunikasi, radar, pemrosesan sinyal audio, sensor frekuensi, dan pengembangan jaringan nirkabel.</p>
+                    <p>peneliti sinyal/frekuensi merupakan profesional yang berfokus pada pengkajian, analisis, dan pengembangan teknologi berbasis gelombang elektromagnetik, sinyal analog, dan sinyal digital. Bidang penelitian ini mencakup berbagai aplikasi, seperti telekomunikasi, radar, pemrosesan sinyal audio, sensor frekuensi, dan pengembangan jaringan nirkabel.</p>
                 </div>
             </div>
         </section>
